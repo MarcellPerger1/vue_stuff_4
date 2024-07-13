89303787f037716dcb3c13a8ced4771e9ae992b8
Marcell Perger <marcell.perger.1@gmail.com>
2024-07-13 20:44:48 +0100

    Refactor AppV2 a bit

--- src/AppV2.vue
+++ src/AppV2.vue
@@ -1,31 +1,30 @@
 <template>
   <div class="rt">
     <div class="file-item">
-      <div class="ico-container">
-        <img class="ic" src="./assets/logo.svg" />
+      <div class="file-icon-container">
+        <img src="./assets/logo.svg" />
       </div>
       <div class="fsobj-name file-name">
-        Hello World. abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.vue
+        filename.vue
       </div>
     </div>
   </div>
 </template>
 
 <style>
-.ico-container {
+.file-icon-container {
   display: grid;
   place-content: center;
 }
-.ico-container > * {
-  width: 1.2em;
-  height: 1.2em;
+.file-icon-container > * {
+  width: 1.1em;
+  height: 1.1em;
 }
-.ico-container > img {
+.file-icon-container > img {
   object-fit: contain;
 }
 
 .file-item {
-  font-size: 16px;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -34,7 +33,7 @@
 }
 
 .file-name {
-  margin-left: 1em;
+  margin-left: 0.5em;
   height: 1em;
   line-height: 1em;
 }
