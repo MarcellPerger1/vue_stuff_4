2097f84cab615669342459678663a58aa5e4b9d6
Marcell Perger <marcell.perger.1@gmail.com>
2024-07-13 21:34:51 +0100

    refactor: Rename variable

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import App2 from "./App.vue";
+import App from "./App.vue";
 
 import "./assets/main.css";
 
-createApp(App2).mount("#app");
+createApp(App).mount("#app");
