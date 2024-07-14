7733491c3797a8b8b701d9ebcd0b82c6759346ee
Marcell Perger <marcell.perger.1@gmail.com>
2024-07-14 21:55:32 +0100

    Add `Add File` button

---
+++ src/components/AddFileButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineEmits(['add-file']);
+</script>
+
+<template>
+  <div class="add-file-button">
+    <button @click.left="$emit('add-file')">+ Add File</button>
+  </div>
+</template>
+
+<style scoped>
+.add-file-button {
+  padding-left: 0.4em;
+}
+</style>
